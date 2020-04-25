c3c9404b8132560876a13729f708cbf9915f3093
isaac-3 <isaacgc0596@gmail.com>
2020-04-25 12:19:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+// function writeCards(["Ada", "Brendan", "Ali"], "birthday"){
+//     for (let i = 0; i < )
+// }
+
+// const person = ["Ada", "Brendan", "Ali"]
+// function writeCards(person){
+//     for(let i = 0; i < person.length; i++){
+//         return(`Thank you, ${i}, for the wonderful birthday gift!`)
+//     }
+// }
+function writeCards(names, event){
+    let cards = []
+    for(let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown(num){
+    while (num > 0){
+        console.log(num)
+        num -= 1
+    }
+    console.log(num)
+}
+
